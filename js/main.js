407544d81b30d8ed10333d3f2c96058b07e82445
Letalandroid <carlossoncra@protonmail.com>
2021-12-31 15:52:50 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("pickear").addEventListener("click", () => {
+
+    let colorElegido = document.getElementById("colorElegido").value;
+
+    document.body.style.backgroundColor = colorElegido;
+
+})
